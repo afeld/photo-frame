1a9f05f25a4cfd6796b298d2364b2bd7abde09f9
Aidan Feldman <aidan.feldman@gmail.com>
2018-12-12 03:55:57 -0500

    refactor the responsive image functions

--- src/Img.tsx
+++ src/Img.tsx
@@ -6,27 +6,30 @@ interface Props {
 }
 
 export default class Img extends Component<Props> {
+  sizeStr(img: pf.Image, i: number, images: pf.Image[]) {
+    // media condition "must be omitted for the last item"
+    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#attr-sizes
+    if (i === images.length - 1) {
+      return `${img.width}w`;
+    }
+    return `(max-width: ${img.width}px and max-height: ${img.height}px) ${
+      img.width
+    }w`;
+  }
+
+  srcsetStr(img: pf.Image) {
+    return `${img.source} ${img.width}w`;
+  }
+
   render() {
     const photo = this.props.photo;
     const images = photo.webp_images;
+    // the largest one - they are in descending order
     const url = images[0].source;
 
     // make images responsive
-    const sizes = images
-      .map((img, i) => {
-        if (i === images.length - 1) {
-          return `${img.width}w`;
-        }
-        return `(min-width: ${img.width}px and min-height: ${img.height}px) ${
-          img.width
-        }w`;
-      })
-      .join(", ");
-    const srcset = images
-      .map((img, i) => {
-        return `${img.source} ${img.width}w`;
-      })
-      .join(", ");
+    const sizes = images.map(this.sizeStr).join(", ");
+    const srcset = images.map(this.srcsetStr).join(", ");
 
     return (
       <img

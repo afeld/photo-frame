d54afd93cf51e6088e8541e43e3a06a5f9997b70
Aidan Feldman <aidan.feldman@gmail.com>
2018-12-21 03:01:38 -0500

    refactor photo fetching into smaller functions

--- src/Photos.ts
+++ src/Photos.ts
@@ -13,6 +13,13 @@ interface PhotosResponse {
   error?: FBError;
 }
 
+const photosFromUser = (response: PhotosResponse) => {
+  if (response.error) {
+    console.error(response.error.message);
+  }
+  return response.data || [];
+};
+
 const onPhotosFetched = (responses: FBBatchResponse[]) => {
   const photos = responses.reduce(
     (acc, response) => {
@@ -24,13 +31,8 @@ const onPhotosFetched = (responses: FBBatchResponse[]) => {
       // there is one key+value pair per user
       const photosResponses = Object.values(json) as PhotosResponse[];
       return photosResponses.reduce((innerAcc, photosRes) => {
-        if (photosRes.error) {
-          console.error(photosRes.error.message);
-        }
-        if (!photosRes.data) {
-          return innerAcc;
-        }
-        return innerAcc.concat(photosRes.data);
+        const photos = photosFromUser(photosRes);
+        return innerAcc.concat(photos);
       }, acc);
     },
     [] as pf.Photo[]
@@ -43,6 +45,7 @@ export function getPhotos(
   FB: fb.FacebookStatic,
   cb: (photos: pf.Photo[]) => void
 ) {
+  const photoFields = "name,webp_images";
   // batch requests
   // https://stackoverflow.com/a/16001318/358804
   // https://developers.facebook.com/docs/graph-api/making-multiple-requests#operations
@@ -61,14 +64,12 @@ export function getPhotos(
         // tagged photos
         {
           method: "GET",
-          relative_url:
-            "photos?fields=name,webp_images&ids=me,{result=friends:$.data.*.id}"
+          relative_url: `photos?fields=${photoFields}&ids=me,{result=friends:$.data.*.id}`
         },
         // uploaded photos
         {
           method: "GET",
-          relative_url:
-            "photos?type=uploaded&fields=name,webp_images&ids=me,{result=friends:$.data.*.id}"
+          relative_url: `photos?type=uploaded&fields=${photoFields}&ids=me,{result=friends:$.data.*.id}`
         }
       ]
     },

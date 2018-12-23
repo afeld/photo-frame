4750e4e130bb8c16d3708d39453f6b088c03872a
Aidan Feldman <aidan.feldman@gmail.com>
2018-12-23 00:23:15 -0500

    add login instructions

--- src/FBLogin.tsx
+++ src/FBLogin.tsx
@@ -28,17 +28,29 @@ export default class FBLogin extends Component<Props> {
   render() {
     return (
       <div className="fb-login-wrapper">
-        <div
-          className="fb-login-button"
-          data-auto-logout-link="false"
-          data-button-type="login_with"
-          data-max-rows="1"
-          data-onlogin="checkLoginStatus();"
-          data-scope={Array.from(SCOPES).join(",")}
-          data-show-faces="false"
-          data-size="large"
-          data-use-continue-as="false"
-        />
+        <ol>
+          <li>
+            Get Aidan to add you to Testers list (not publicly available yet)
+          </li>
+          <li>
+            <a href="https://developers.facebook.com/requests">
+              Accept the App request
+            </a>
+          </li>
+          <li>
+            <div
+              className="fb-login-button"
+              data-auto-logout-link="false"
+              data-button-type="login_with"
+              data-max-rows="1"
+              data-onlogin="checkLoginStatus();"
+              data-scope={Array.from(SCOPES).join(",")}
+              data-show-faces="false"
+              data-size="large"
+              data-use-continue-as="false"
+            />
+          </li>
+        </ol>
       </div>
     );
   }

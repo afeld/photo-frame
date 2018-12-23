fccce1434ee6ca7e77f87f7bc6a63945d7ab91e5
Aidan Feldman <aidan.feldman@gmail.com>
2018-12-23 00:57:18 -0500

    expand app info

--- src/Info.tsx
+++ src/Info.tsx
@@ -25,6 +25,14 @@ export default class Info extends Component<Props> {
           />
         </a>
         <p>This is Aidan Feldman's Photo Frame app.</p>
+        <p>
+          The app will show Facebook photos that you have uploaded or are tagged
+          in, as well as those from any friends that authorize the app.
+        </p>
+        <p>
+          Source code is available{" "}
+          <a href="https://github.com/afeld/photo-frame">here</a>.
+        </p>
       </div>
     );
   }

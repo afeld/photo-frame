9e7a6d73221b182c668f86690475f249a18e5f5a
Aidan Feldman <aidan.feldman@gmail.com>
2018-12-22 22:46:40 -0500

    fix App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 it("renders without crashing", () => {
+  const FB = {
+    api: jest.fn(),
+    AppEvents: { logPageView: jest.fn() },
+    Canvas: {},
+    Event: {},
+    getAuthResponse: jest.fn(),
+    getLoginStatus: jest.fn(),
+    init: jest.fn(),
+    login: jest.fn(),
+    logout: jest.fn(),
+    ui: jest.fn(),
+    XFBML: {}
+  };
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+
+  ReactDOM.render(<App FB={FB} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });

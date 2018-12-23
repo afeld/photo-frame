54c6f4a73213236487b52726c12bf672db374e6e
Aidan Feldman <aidan.feldman@gmail.com>
2018-12-22 23:31:55 -0500

    use more explicit response error typing

--- src/Photos.ts
+++ src/Photos.ts
@@ -9,21 +9,31 @@ interface FBError {
   message: string;
 }
 
+interface FBErrorResponse {
+  error: FBError;
+}
+
 interface FBBatchOpResponse {
-  error?: FBError;
-  [userId: string]: any; // should actually be PhotosResponse but TypeScript gives an error
+  [userId: string]: PhotosResponse;
 }
 
 interface PhotosResponse {
-  data?: pf.Photo[];
-  error?: FBError;
+  data: pf.Photo[];
 }
 
-const photosFromUser = (response: PhotosResponse) => {
-  if (response.error) {
+// https://www.typescriptlang.org/docs/handbook/advanced-types.html#user-defined-type-guards
+const isErrorResponse = (
+  response: FBErrorResponse | any
+): response is FBErrorResponse => {
+  return (<FBErrorResponse>response).error !== undefined;
+};
+
+const photosFromUser = (response: FBErrorResponse | PhotosResponse) => {
+  if (isErrorResponse(response)) {
     console.error(response.error.message);
+    return [];
   }
-  return response.data || [];
+  return response.data;
 };
 
 const photosFromBatch = (response: FBBatchResponse | null) => {
@@ -31,8 +41,8 @@ const photosFromBatch = (response: FBBatchResponse | null) => {
   if (!response) {
     return [];
   }
-  const json = JSON.parse(response.body) as FBBatchOpResponse;
-  if (json.error) {
+  const json = JSON.parse(response.body) as FBBatchOpResponse | FBErrorResponse;
+  if (isErrorResponse(json)) {
     console.error(json.error.message);
     return [];
   }

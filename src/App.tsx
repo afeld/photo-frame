f1e3b92f2767380fa70a6197edf181b236f29448
Aidan Feldman <aidan.feldman@gmail.com>
2018-12-29 04:37:46 -0500

    support screen orientation changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { Facebook } from "expo";
+import { Facebook, ScreenOrientation } from "expo";
 import Carousel from "./Carousel";
 import Login from "./Login";
 import { saveToken, fetchToken } from "./TokenStore";
+
+declare module "expo" {
+  namespace ScreenOrientation {
+    // workaround for missing function, which will be obviated by
+    // https://github.com/expo/expo/issues/2164
+    function allowAsync(orientation: keyof Orientations): Promise<null>;
+  }
+}
 
 export default class App extends React.Component {
   state = { token: null as string };
 
   constructor(props) {
     super(props);
+    ScreenOrientation.allowAsync(
+      ScreenOrientation.Orientation.ALL_BUT_UPSIDE_DOWN
+    );
     this.fetchToken();
   }
 
--- src/Img.tsx
+++ src/Img.tsx
@@ -6,16 +6,34 @@ interface Props {
   img: pf.Image;
 }
 
-export default class Img extends React.Component<Props> {
-  render() {
+interface State {
+  height: number;
+  width: number;
+}
+
+export default class Img extends React.Component<Props, State> {
+  componentWillMount() {
+    this.updateDimensions();
+    Dimensions.addEventListener("change", this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    Dimensions.removeEventListener("change", this.updateDimensions);
+  }
+
+  updateDimensions = () => {
     const { height, width } = Dimensions.get("window");
+    this.setState({ height, width });
+  };
+
+  render() {
     const uri = this.props.img.source;
 
     return (
       <Image
         style={{
-          width,
-          height,
+          width: this.state.width,
+          height: this.state.height,
           resizeMode: "contain",
           backgroundColor: "black"
         }}
